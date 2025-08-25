4a26576f0b1589d126c936103ab35f7f031a1dc5
chico <105537798+eltrul@users.noreply.github.com>
2025-08-25 19:00:22 +0700

    Create table.lua

---
+++ table.lua
@@ -0,0 +1,74 @@
+setreadonly(table, false)
+table.remove = function(tbl, key) 
+    local Result = {} 
+    for Index, Value in tbl do 
+        if Index ~= key then 
+            table.insert(Result, Value) 
+        end 
+    end 
+    
+    tbl = Result
+end 
+
+table.removeKey = function(tbl, key) 
+    local Result = {} 
+    for Index, Value in tbl do 
+        if Index ~= key then 
+            Result[Index] = Value
+        end 
+    end 
+    
+    tbl = Result
+end 
+
+table.length = function(tbl) 
+    local Count = 0 
+    for _ in tbl do 
+        Count = Count + 1 
+    end 
+    
+    return Count
+end 
+
+table.keys = function(tbl) 
+    local Result = {} 
+    
+    for i in tbl do 
+        table.insert(Result, i) 
+    end 
+    
+    return Result
+end
+
+table.values = function(tbl)
+    local Result = {} 
+    
+    for _, i in tbl do 
+        table.insert(Result, i) 
+    end 
+    
+    return Result
+end
+
+table.reverse = function(tbl) 
+    local Result = {} 
+    
+    for u, i in ipairs(tbl) do 
+        
+        Result[i] = u
+        
+    end 
+    
+    return Result
+end
+
+
+table.sum = function(tbl) 
+    a = 0 
+    for _, v in tbl do
+        a = a + v 
+    end 
+    return a
+end 
+
+setreadonly(table, true)
