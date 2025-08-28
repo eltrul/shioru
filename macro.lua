1296eb6728d42012961128929c47c70385afff60
chico <105537798+eltrul@users.noreply.github.com>
2025-08-28 16:39:35 +0700

    Create macro.lua

---
+++ macro.lua
@@ -0,0 +1,65 @@
+--[[local args = {
+	"unit_tomato_plant",
+	{
+		Valid = true,
+		Rotation = 180,
+		CF = CFrame.new(-787.47607421875, 61.93030548095703, -213.0801544189453, -1, 0, -8.742277657347586e-08, 0, 1, 0, 8.742277657347586e-08, 0, -1),
+		Position = vector.create(-787.47607421875, 61.93030548095703, -213.0801544189453)
+	}
+game:GetService("ReplicatedStorage"):WaitForChild("RemoteFunctions"):WaitForChild("PlaceUnit"):InvokeServer(unpack(args))
+}]] 
+JSON = loadstring(game:HttpGet('https://raw.githubusercontent.com/eltrul/shioru/refs/heads/main/json.lua'))() 
+
+MacroRecorder = {
+    Callbacks = {} 
+} 
+
+function MacroRecorder.StartRecording () 
+    MacroRecorder.Keyframes = {}
+    MacroRecorder.LastEmitted = tick()
+    MacroRecorder.IsRecording = true 
+end 
+
+function MacroRecorder.StopRecording () 
+    MacroRecorder.IsRecording = false 
+end
+
+function MacroRecorder.GetRecordData () 
+    return JSON.stringify(MacroRecorder.Keyframes)
+end 
+
+function MacroRecorder.SubmitData (data) 
+    MacroRecorder.Keyframes.push({
+        type = 'wait', 
+        data = tick() - MacroRecorder.LastEmitted
+    }) 
+    MacroRecorder.Keyframes.push({
+        type = data.type, 
+        data = data.data
+    })
+    MacroRecorder.LastEmitted = tick()
+    for i, v in MacroRecorder.Callbacks do 
+        if v[1] == 'NewKeyframeSubmitted' then 
+            v[2](data)
+        end 
+    end 
+end 
+
+function MacroRecorder.On(EventType, Callback) 
+    table.insert(MacroRecorder.Callbacks, {
+        EventType,
+        Callbacks
+    })
+end 
+hookmetamethod(game, '__namecall', newcclosure(function(self, ...)
+    if self and typeof(self) == 'Instance' and tostring(self.Parent) == 'RemoteFunctions' then 
+        if MacroRecorder.IsRecording then 
+            MacroRecorder.SubmitData({
+                type = getnamecallmethod(), 
+                data = {...}
+            })
+        end 
+    end 
+end))
+
+return MacroRecorder
