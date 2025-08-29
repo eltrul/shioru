e9d167c96a8b3792ea40c248b5deb97ffe958ad9
chico <105537798+eltrul@users.noreply.github.com>
2025-08-29 19:07:04 +0700

    Update json.lua

--- json.lua
+++ json.lua
@@ -1,38 +1,17 @@
 local HttpService = game:GetService("HttpService")
-JSON = {}
+
+local JSON = {}
+
+-- Encoders
 local function encodeVector3(v)
-	return {__type = "Vector3", x = v.X, y = v.Y, z = v.Z}
+	return { __type = "Vector3", x = v.X, y = v.Y, z = v.Z }
 end
 
 local function encodeCFrame(cf)
-	local components = {cf:GetComponents()}
-	return {
-		__type = "CFrame",
-		components = components
-	}
+	return { __type = "CFrame", components = { cf:GetComponents() } }
 end
 
-function JSON.stringify(data)
-	local result = {}
-
-	for key, value in pairs(data) do
-	    print('render', keu, value, typeof(value))
-		if typeof(value) == "Vector3" then
-		    print('IsA: Vector3')
-			result[key] = encodeVector3(value)
-		elseif typeof(value) == "CFrame" then
-		    print('IsA: CFrame')
-			result[key] = encodeCFrame(value)
-		elseif typeof(value) == 'table' then 
-		    result[key] = JSON.stringify(value) 
-		else 
-			result[key] = value
-		end
-	end
-
-	return HttpService:JSONEncode(result)
-end
-
+-- Decoders
 local function decodeVector3(t)
 	return Vector3.new(t.x, t.y, t.z)
 end
@@ -41,21 +20,53 @@ local function decodeCFrame(t)
 	return CFrame.new(unpack(t.components))
 end
 
-function JSON.parse(json)
-	local result = {}
-	local data = HttpService:JSONDecode(json)
+-- Recursive serializer
+local function serialize(value)
+	local valueType = typeof(value)
 
-	for key, value in pairs(data) do
-		if typeof(value) == "table" and value.__type == "Vector3" then
-			result[key] = decodeVector3(value)
-		elseif typeof(value) == "table" and value.__type == "CFrame" then
-			result[key] = decodeCFrame(value)
-		else
-			result[key] = value
+	if valueType == "Vector3" then
+		return encodeVector3(value)
+	elseif valueType == "CFrame" then
+		return encodeCFrame(value)
+	elseif valueType == "table" then
+		local result = {}
+		for k, v in pairs(value) do
+			result[k] = serialize(v)
 		end
+		return result
+	else
+		return value
 	end
+end
 
-	return result
+-- Recursive deserializer
+local function deserialize(value)
+	if typeof(value) == "table" then
+		if value.__type == "Vector3" then
+			return decodeVector3(value)
+		elseif value.__type == "CFrame" then
+			return decodeCFrame(value)
+		else
+			local result = {}
+			for k, v in pairs(value) do
+				result[k] = deserialize(v)
+			end
+			return result
+		end
+	else
+		return value
+	end
+end
+
+-- Public functions
+function JSON.stringify(data)
+	local serialized = serialize(data)
+	return HttpService:JSONEncode(serialized)
+end
+
+function JSON.parse(json)
+	local decoded = HttpService:JSONDecode(json)
+	return deserialize(decoded)
 end
 
 return JSON
